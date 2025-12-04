6be14d901f8fa738a8508d09596a263e44b54091
amine <aminebichiou2@gmail.com>
2025-12-04 11:10:35 +0100

    test

--- src/main/java/com/example/event/repositories/EventRepository.java
+++ src/main/java/com/example/event/repositories/EventRepository.java
@@ -3,4 +3,5 @@ import com.example.event.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    
 }
